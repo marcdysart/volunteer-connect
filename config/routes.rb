515fb104f8a315f01cd839b2b29d546cf5a34f73
marcdysart <dysart.marc@gmail.com>
2015-01-07 13:00:22 -0700

    setup haml and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
+  get 'profile' => 'welcome#about'
 
     root to: 'welcome#index'
 end
