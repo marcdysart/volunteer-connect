910591f7ca21bd3c2f522cd5971dfa658c59d37e
marcdysart <dysart.marc@gmail.com>
2015-01-13 17:07:31 -0700

    Add an image to a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :user
+  mount_uploader :image, ImageUploader
 
   default_scope { order('created_at DESC') }
 end
