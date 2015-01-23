d23f7c121c20087ea0b5589aab599fdc225e3bf9
marcdysart <dysart.marc@gmail.com>
2015-01-23 14:21:34 -0700

    Add Browse by User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.visible_to(current_user)
+    @posts = @user.posts
     @comments = @user.comments
   end
 
